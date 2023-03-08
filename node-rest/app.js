fd0a48665102caaee7e8ef20cf2ffb526569e678
cent kanayo <centkanayo@gmail.com>
2023-03-08 13:08:36 +0100

    New class 08/03/23

--- node-rest/app.js
+++ node-rest/app.js
@@ -13,6 +13,15 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
+const productSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+});
+const Product = mongoose.model('Product', productSchema);
+
+// connection
+
 mongoose
   .connect(process.env.DBCONNECT)
   .then(() => {
@@ -35,11 +44,71 @@ app.get('/api/v1', (request, response) => {
   response.status(200).json({ status: 'Success', result: [] });
 });
 
-app.get('/api/v1/products', (req, res) => {
-  res.status(200).json({ status: 'Success', result: [] });
+app.get('/api/v1/products', async (req, res) => {
+  const products = await Product.find();
+  if (!products.length) {
+    res.status(200).json({ status: 'Fail', message: 'No products yet' });
+  }
+  res.status(200).json({ Status: 'Success', result: products });
 });
-app.get('/api/v1/products/:id', (req, res) => {
-  res.json({ status: 'Success', result: [] });
+app.get('/api/v1/products/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res
+        .status(404)
+        .json({ status: 'Fail', message: `No product with matching id ${id}` });
+    }
+    res.status(200).json({ status: 'Success', result: product });
+  } catch (error) {
+    res
+      .status(404)
+      .json({ status: 'Fail', message: `No product with matching id ${id}` });
+  }
+});
+
+app.post('/api/v1/add-product', async (req, res) => {
+  const { title, description, price } = req.body;
+  if (!title || !description || !price) {
+    res.status(400).json({ Status: 'Fail', message: 'All field are required' });
+  }
+  const newProduct = await Product.create({ title, description, price });
+  res.status(201).json({ Status: 'Success' });
+});
+app.patch('/api/v1/update-product/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res
+        .status(404)
+        .json({ status: 'Fail', message: `No product with matching id ${id}` });
+    }
+    // product.price = req.body.price || product.price;
+    // product.description = req.body.description || product.description;
+    // product.title = req.body.title || product.title;
+    // product.save();
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      { _id: id },
+      req.body,
+      {
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json({
+      Status: 'Success',
+      message: `Product with id ${id} has been updated`,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'Fail',
+      message: `No product with matching id ${id}`,
+    });
+  }
 });
 
 app.post('/api/v1/register', async (req, res) => {
